bbca4d0079605223d56386959d2f0667f47a647a
senno adi <senno.adi2509@gmail.com>
2022-10-25 18:30:21 +0700

    init demo

---
+++ lib/unique_file/style_dictionary_color.dart
@@ -0,0 +1,34 @@
+
+//
+// style_dictionary_color.dart
+//
+
+// Do not edit directly
+// Generated on Tue, 25 Oct 2022 11:30:01 GMT
+
+
+
+import 'dart:ui';
+
+class adrColor {
+  adrColor._();
+
+    static const backgroundBase = Color(0xFFF2F4F7);
+    static const backgroundDark = Color(0xFF1D2939);
+    static const backgroundDisable = Color(0xFFFCFCFD);
+    static const backgroundError = Color(0xFFF04438);
+    static const backgroundLight = Color(0xFFFCFCFD);
+    static const backgroundLink = Color(0xFFF5FAFF);
+    static const backgroundPrimary = Color(0xFFFFDD00);
+    static const backgroundPrimaryLight = Color(0xFFFEF5CB);
+    static const backgroundSuccess = Color(0xFF12B76A);
+    static const outlineButtonSecondary = Color(0xFFFCFCFD);
+    static const outlineDisable = Color(0xFF98A2B3);
+    static const outlineGray = Color(0xFF98A2B3);
+    static const outlineLink = Color(0xFFB2DDFF);
+    static const typeButton = Color(0xFF344054);
+    static const typeLink = Color(0xFF1570EF);
+    static const typeNormal = Color(0xFF1D2939);
+    static const typePlaceholder = Color(0xFF98A2B3);
+    static const typeWhite = Color(0xFFFCFCFD);
+}
---
+++ lib/unique_file/style_dictionary_shape.dart
@@ -0,0 +1,34 @@
+
+//
+// style_dictionary_shape.dart
+//
+
+// Do not edit directly
+// Generated on Tue, 25 Oct 2022 11:30:01 GMT
+
+
+
+import 'dart:ui';
+
+class adrShape {
+  adrShape._();
+
+    static const buttonBorder = 2;
+    static const buttonButtonGap = 8;
+    static const buttonButtonPadSide = 24;
+    static const buttonButtonPadTop = 8;
+    static const buttonButtonPadTopVariation = 12;
+    static const buttonOutlinedRadius = 100.0;
+    static const buttonPrimaryRadius = 100.0;
+    static const buttonSidebarGap = 8;
+    static const buttonSidebarPadAll = 4;
+    static const buttonSidebarPadSide = 16;
+    static const buttonSidebarRadius = 12.0;
+    static const buttonTextRadius = 100.0;
+    static const displayCardsRadius = 12.0;
+    static const displayDialogsRadius = 28.0;
+    static const displayTabsRadius = 8.0;
+    static const inputCheckboxRadius = 4.0;
+    static const inputChipsRadius = 4.0;
+    static const inputFieldRadius = 4.0;
+}
